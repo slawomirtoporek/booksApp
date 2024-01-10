8e3b536753aa0436714cb2a9ba1b42cec1ef2e4b
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-01-10 21:43:06 +0100

    add the ability to unlike a book in the initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -9,16 +9,23 @@
     book: {
       image: '.book__image',
       id: 'data-id',
-    }
+    },
   };
 
   const classNames = {
+    library: {
+      imageFavorite: 'favorite',
+    },
+  };
+
+  const templates = {
     library: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
   };
+  
 
   function render () {
     for (let book of dataSource.books) {
-      const generatedHTML = classNames.library(book);
+      const generatedHTML = templates.library(book);
       const elementDOM = utils.createDOMFromHTML(generatedHTML);
       const booksContainer = document.querySelector(select.containerOf.listBooks);
       booksContainer.appendChild(elementDOM);
@@ -33,9 +40,20 @@
     for (let selectBook of books) {
       selectBook.addEventListener('dblclick', function(event) {
         event.preventDefault(); 
-        selectBook.classList.add('favorite');
+
         const bookId = selectBook.getAttribute(select.book.id);
-        favoriteBooks.push(bookId);
+
+        if (favoriteBooks.includes(bookId)) {
+          selectBook.classList.remove(classNames.library.imageFavorite);
+          const index = favoriteBooks.indexOf(bookId);
+          if (index != -1) {
+            favoriteBooks.splice(index, 1);
+          }
+        } else {
+          selectBook.classList.add(classNames.library.imageFavorite);
+          favoriteBooks.push(bookId);
+        }
+        console.log({favoriteBooks});
       });
     }
   }
